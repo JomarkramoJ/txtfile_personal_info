8036b8e87331dd6c2e41bc222a853fd78b559d83
Jomark <maguadjomarkglenn@gmail.com>
2024-11-26 04:38:07 +0800

    printing

--- read.py
+++ read.py
@@ -1,8 +1,8 @@
-def find_user(file_path, user_info):
+def find_user(file_path, search_name):
     try:
         with open(file_path, "r") as file:
             for line in file:
-                user_info = eval(line.strip)
+                user_info = eval(line.strip())
                 if user_info.get("Name").lower() == search_name.lower():
                     return user_info
         return None
@@ -12,3 +12,11 @@ def find_user(file_path, user_info):
 
 file_path = "./information_list.py"
 search_name = input("Enter the full name to search: ").strip()
+
+result = find_user(file_path, search_name)
+if result:
+    print("\nInformation found: ")
+    for key, value in result.items():
+        print(f"{key}: {value}")
+else:
+    print("\nNo information found for the given name.")
